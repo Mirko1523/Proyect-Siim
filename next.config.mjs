407e654b421f1a53af4351bea19a76cade28eea4
Mirko1523 <milermirko1@gmail.com>
2025-05-30 10:10:14 -0300

    the last commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ————————————————
+  // 1) Activa el modo “export”:
+  output: 'export',
+
+  // 2) (Opcional pero recomendado)
+  //    Si quieres que cada ruta vaya a una carpeta
+  //    /about → /about/index.html
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
